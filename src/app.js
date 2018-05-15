cc1a6482a16d4c9756eb76f628b1ce4be3b906b1
chenqu <chenqu@meituan.com>
2018-05-15 17:22:29 +0800

    add

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 // const Vue = require('vue');
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        title: '这是一个title',
+        messages: [
+            1,3,4,5
+        ],
+        inputValue: 'hello vue',
+        groceryList: [
+            { id: 0, text: '蔬菜' },
+            { id: 1, text: '奶酪' },
+            { id: 2, text: '随便其它什么人吃的东西' }
+        ]
+    },
+    methods: {
+        reverseMessage: function() {
+            this.messages = this.messages.reverse();
+        }
+    },
+    components: {
+
     }
 })
