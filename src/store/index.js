cac692a2b6ca2d86b92f59bc8e170576fd07612f
chenqu <chenqu@meituan.com>
2018-05-21 20:12:40 +0800

    dd

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import modulesAll from './modules';
 Vue.use(Vuex);
 export const store = new Vuex.Store({
+    strict: true, // 开启严格模式
     modules: {
         ...modulesAll
     }
